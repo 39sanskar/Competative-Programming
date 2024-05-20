25b978027b901aefb1ea8359b23722d046b5dc83
39sanskar <mishrasanskar496@gmail.com>
2024-05-20 20:59:04 +0530

    scratch1.cpp

---
+++ scratch1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+// int main(){
+
+//     int i =0;
+//     while(i<10){ // while loop is only contain condition for the middle part that has the condition has STATEMENT- 2 // while loop is only contain condition for the middle part that has the condition has STATEMENT- 2
+//         cout<<i<<endl;
+//         i++;
+//     }
+    
+// }
+
+// for the do-while loop is always contain the STATEMENT- 1 and the condition has STATEMENT- 2
+//and also need the semicolon at the end ;
+int main(){
+    int n; cin >> n;
+    int i = 0;
+
+    do{
+        cout << i << endl;
+        i++;
+    }while(i<n);
+
+
+}
+
+
