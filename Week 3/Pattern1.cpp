77b538e1276bb2895e34020015f5bbb964dffc49
39sanskar <mishrasanskar496@gmail.com>
2024-05-21 22:31:08 +0530

    Using * symbol print a Solid Recytangle.

---
+++ Week 3/Pattern1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Solid Rectangle.
+int main(){
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i <= n; i++)//outer loop it is actually printing every row.
+    {
+
+        for( int j = 0; j <= m; j++)// inner loop it is actually printing every column.
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
