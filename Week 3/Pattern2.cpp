7b59d7fd84440d0a9f72ed25842b9f71d2786b60
39sanskar <mishrasanskar496@gmail.com>
2024-05-21 23:21:52 +0530

    Using * symbol to print the Holllow rectangle.

---
+++ Week 3/Pattern2.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+//Hollow Rectangle.
+int main(){
+    int n, m;
+    cin >> n >> m;
+    for ( int i = 1; i<=n; i++)
+    {
+        for( int j = 1; j<=m; j++)
+        {
+            if(i == 1 || i == n)
+            {
+               cout << "*";
+            }//i stand at the first or last row.
+            else
+            {
+               if( j == 1 || j == m) 
+               cout << "*";
+               else cout << " ";
+            }
+            
+            
+        }
+        cout << endl;
+    }
+    return 0;
+
+}
