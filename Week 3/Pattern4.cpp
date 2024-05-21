29e695302ed92891cac36b55ec531c5d13a592e8
39sanskar <mishrasanskar496@gmail.com>
2024-05-22 00:37:51 +0530

    Inverted Triangle

---
+++ Week 3/Pattern4.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, m;
+    cin >> n >> m;
+    for(int i =1; i <= n; i++)//Outer loop
+    {
+       for( int j =1; j <=(n-i+1); j++)// Inner loop
+       {
+        cout << "*";
+       }
+       cout<<endl;
+    }
+    return 0;
+}
