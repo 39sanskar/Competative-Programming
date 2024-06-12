75856911f5bf896e5dab26b2a51f7889787b4218
Sanskar Mishra <152903486+39sanskar@users.noreply.github.com>
2024-06-12 22:10:51 +0530

    Update Problem13.cpp
    Something new

--- Week 3/Problem13.cpp
+++ Week 3/Problem13.cpp
@@ -7,7 +7,6 @@
 // Output:
 // * ** ***
 // Explanation:
-// Explanation:
 // First, print 1 asterisk then space after
 // that print 2 asterisk and space after that 
 // print 3 asterisk now stop as N is 3.
