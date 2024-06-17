d0e45091edfbc04865a213f4c4c7233b6f482603
39sanskar <mishrasanskar496@gmail.com>
2024-06-18 00:20:07 +0530

    beautiful-year

--- Week 4/Problem1.cpp
+++ Week 4/Problem1.cpp
@@ -17,3 +17,37 @@
 // 2013
 // outputCopy
 // 2014
+#include<iostream>
+using namespace std;
+int main(){
+    int y ;
+    cin >> y;
+    for( int i=y+1; i<10000; i++){
+        int freq[10];// stores the frequency of every digit in the i.
+        for (int j =0; j<10; j++){
+            freq[j] = 0;
+
+
+        }
+        // memset(freq,0,sizeof(freq))
+        int year=i;
+        while(year>0){
+            int digit = year%10;
+            freq[digit]++;
+            // year/=10;
+            year = year/10;
+        }
+        int flag = 0;// indicates that if any occured more than 1 time.
+        for(int i=0; i<10; i++){
+            if(freq[i]>1){
+                flag=1;
+                break;
+
+            }
+        }
+        if(flag==0){
+            cout<<i<<endl;
+            break;
+        }
+    }
+}
