c46717f27544a0a3adecebc43b9827c296b4f221
39sanskar <mishrasanskar496@gmail.com>
2024-06-30 18:33:56 +0530

    Dust Sweeper

--- Week 4/Problem7.cpp
+++ Week 4/Problem7.cpp
@@ -103,5 +103,52 @@
 // , yielding the array [0,0,0,0,4]
 // .
 // In the last case, the array already satisfies the condition.
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n + 1];
+        for(int i=1; i<=n; i++)
+        cin >> arr[i];
+        
+        long long cntZ = 0;
+        long long sum = 0;
+        bool flag = false;
+
+        for(int i = 1; i <= n-1; i++)
+        {
+            sum += arr[i];
+            if (arr[i]>0) flag =true;
+            if (arr[i]==0 && flag ==true) cntZ++;
+        }
+        
+        cout << sum + cntZ << endl;
+
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
