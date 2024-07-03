51f7535680e6d8773d06e0e47adce7a716519312
39sanskar <mishrasanskar496@gmail.com>
2024-07-03 19:26:36 +0530

    Fill the gaps

--- Week 4/Problem11.cpp
+++ Week 4/Problem11.cpp
@@ -177,6 +177,24 @@ int main(){
     return 0;
 }
 
+//Another way to solce the Problem
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &x : a) cin >> x;
+
+    for (int i = 1; i < a.size(); i++) {
+        if (a[i-1]+1 < a[i]) a.insert(a.begin()+i, a[i-1]+1);
+        if (a[i-1]-1 > a[i]) a.insert(a.begin()+i, a[i-1]-1);
+    }
+    for (auto x : a) cout << x << ' ';
+}
+
 
 
 
