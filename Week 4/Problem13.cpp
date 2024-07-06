93fd7b44e1490f8ccf84d4c45b2b92b054cfd9fd
39sanskar <mishrasanskar496@gmail.com>
2024-07-06 17:26:46 +0530

    Harmonic-Resonance-Value

--- Week 4/Problem13.cpp
+++ Week 4/Problem13.cpp
@@ -1,46 +1,89 @@
-// It seems like the year of 2013 came only yesterday. Do you know a curious fact? The year of 2013 is the first year after the old 1987 with only distinct digits.
+// B. Harmonic Resonance Value
+// time limit per test2 s.
+// memory limit per test256 MB
+// inputstandard input
+// outputstandard output
+// Tushar is a young scientist working on a secret project in the futuristic city of Neo Zenith. He's developing a revolutionary energy synchronizer that requires two power crystals to function. The synchronizer works based on a special property called the Harmonic Resonance Value (HRV) of the two crystals.
 
-// Now you are suggested to solve the following problem: given a year number, find the minimum year number which is strictly larger than the given one and has only distinct digits.
+// The HRV of two crystals with energy levels x
+//  and y
+//  is defined as the smallest energy level that is a multiple of both x
+//  and y
+// . For example, HRV(13,37)=481
+//  and HRV(9,6)=18
+// .
+
+// For each experiment, Tushar is given a range of acceptable energy levels, from l
+//  to r
+// . He needs to find two crystals with energy levels x
+//  and y
+//  that satisfy these conditions:
+
+// 1. Both crystal energy levels are within the given range: l≤x<y≤r
+// 2. The HRV of the two crystals is also within the range: l≤HRV(x,y)≤r
+// Can you help Tushar determine if it's possible to find such crystals for each experiment, and if so, what their energy levels should be?
 
 // Input
-// The single line contains integer y (1000 ≤ y ≤ 9000) — the year number.
+// The first line contains one integer t
+//  (1≤t≤10000
+// ) — the number of experiments Tushar wants to conduct.
+
+// For each experiment: One line containing two integers l
+//  and r
+//  (1≤l<r≤109
+// ) — the lower and upper limits of the acceptable energy range.
 
 // Output
-// Print a single integer — the minimum year number that is strictly larger than y and all it's digits are distinct. It is guaranteed that the answer exists.
+// For each experiment, print two integers:
 
-// Examples
+// If it's impossible to find two crystals meeting all the conditions, print two integers equal to −1
+// ;
+// Otherwise, print the energy levels x
+//  and y
+//  of the two crystals (if there are multiple valid answers, you may print any of them).
+// Example
 // inputCopy
-// 1987
+// 4
+// 1 1337
+// 13 69
+// 2 4
+// 88 89
 // outputCopy
-// 2013
-// inputCopy
-// 2013
-// outputCopy
-// 2014
+// 6 7
+// 14 21
+// 2 4
+// -1 -1
 #include<iostream>
 using namespace std;
+void solve(){
+    int t;
+    cin >>t;
+    for(int i =0; i<t; ++i){
+        int l,r;
+        cin >>l >>r;
+
+        if(2*l <= r){
+            cout << l <<" " << 2*l << endl;
+
+        }
+        else{
+            cout <<" -1 -1" << endl;
+        }
+    }
+}
+
 int main(){
-    int y ;
-    cin >> y;
-    for( int i=y+1; i<10000; i++){
-        int freq[10];// stores the frequency of every digit in the i.
-        for (int j =0; j<10; j++){
-            freq[j] = 0;
+    solve();
+    return 0;
+}
 
 
-        }
-        // memset(freq,0,sizeof(freq))
-        int year=i;
-        while(year>0){
-            int digit = year%10;
-            freq[digit]++;
-            // year/=10;
-            year = year/10;
-        }
-        int flag = 0;// indicates that if any occured more than 1 time.
-        for(int i=0; i<10; i++){
-            if(freq[i]>1){
-                flag=1;
-                break;
 
-            }
+
+
+
+
+
+
+
+
