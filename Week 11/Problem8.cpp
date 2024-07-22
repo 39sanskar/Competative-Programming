24ad8078b7d2685de3de1a57e3a40f74dbc33cba
39sanskar <mishrasanskar496@gmail.com>
2024-07-23 00:25:05 +0530

    B. Make Them Odd

--- Week 11/Problem8.cpp
+++ Week 11/Problem8.cpp
@@ -81,4 +81,43 @@
 // Thus, all numbers became odd after 4
 //  moves. In 3
 //  or fewer moves, you cannot make them all odd.
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
 
+    set<int> st;
+    for(int i=0; i<n; i++)
+    {
+        int x;
+        cin >> x;
+
+        st.insert(x);
+
+    }
+    int ans =0;
+    while(!st.empty())
+    {
+        int val = *st.rbegin();
+        st.erase(val);
+
+        if(val % 2==0)
+        {
+            ans++;
+            st.insert(val / 2);
+        }
+    }
+    
+    cout << ans << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+
+    for(int i = 0; i < t; i++) solve();
+
+    return 0;
+}
