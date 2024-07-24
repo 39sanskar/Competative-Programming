e693cd667fd930b233ac58c941f5635f61ba3a51
39sanskar <mishrasanskar496@gmail.com>
2024-07-24 23:04:46 +0530

    A. Divide and Conquer

--- Week 7/Problem6.cpp
+++ Week 7/Problem6.cpp
@@ -89,6 +89,56 @@
 //  becomes [0]
 // . It can be proved that it is not possible to make a
 //  good in less number of operations.
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int arr[n];
+        int odd = 0;
+        for(int i=0; i<n; i++){
+            cin >> arr[i];
+            if(arr[i]%2 != 0) odd++;
+
+        }
+        int ans = INT_MAX;
+        if(odd%2 == 0){
+            cout <<"0"<< endl;
+
+        }
+        else{
+            for(int i=0; i<n; i++){
+                if(arr[i]%2==0){
+                    int cout=0;
+                    while(arr[i]%2==0){
+                        cout++;
+                        arr[i]/=2;
+
+                    }
+                    ans = min(ans, count);
+
+                }
+                else{
+                    int cout =0;
+                    while(arr[i]%2! = 0){
+                        count++;
+                        arr[i]/=2;
+
+                    }
+                    ans = min(ans, count);
+                }
+            }
+            cout << ans << endl;
+
+        }
+    }
+    return 0;
+}
 
 
 
