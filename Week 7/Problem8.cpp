3e2bb1ec9340ddda9c69a77c05d5ab5f123f27d8
39sanskar <mishrasanskar496@gmail.com>
2024-07-27 19:49:34 +0530

    Adding some information

--- Week 7/Problem8.cpp
+++ Week 7/Problem8.cpp
@@ -34,7 +34,7 @@ using namespace std;
 vector<int> generatePrimes(int n) {
     vector<bool> isPrime(n + 1, true);
     isPrime[0] = false; // 0 is not a prime
-    isPrime[1] = true;  // Including 1 as not a prime number
+    isPrime[1] = true;  // Including 1 as (1 is  not a prime number )
     for (int i = 2; i * i <= n; ++i) {
         if (isPrime[i]) {
             for (int j = i * i; j <= n; j += i) {
