94ca65bd26056d66bc0af378c59dc97110a0a055
Sanskar Mishra <152903486+39sanskar@users.noreply.github.com>
2024-08-09 17:55:17 +0530

    Update Problem12.cpp
    A.A+B Again

--- Week 13/Problem12.cpp
+++ Week 13/Problem12.cpp
@@ -1,1 +1,37 @@
-
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define fastIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL);              \
+    cout.precision(numeric_limits<double>::max_digits10);
+ 
+ 
+void CoderAman()
+{
+    int n;
+    cin >> n;
+    cout << n % 10 + n / 10 << '\n';
+    // int sum = 0;
+    // while (n)
+    // {
+    //     sum += n % 10;
+    //     n /= 10;
+    // }
+    // cout << sum << '\n';
+}
+ 
+int32_t main()
+{
+    fastIO;
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        CoderAman();
+    }
+    return 0;
+}
