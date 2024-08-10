ad062760a76b904a979c1059da0e889dc8c51031
Sanskar Mishra <152903486+39sanskar@users.noreply.github.com>
2024-08-10 17:31:32 +0530

    Update Problem14.cpp
    C.Showering.cpp

--- Week 13/Problem14.cpp
+++ Week 13/Problem14.cpp
@@ -1,1 +1,45 @@
-
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define fastIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL);              \
+    cout.precision(numeric_limits<double>::max_digits10);
+ 
+ 
+void CoderSanskar()
+{
+    int n, s, m;
+    cin >> n >> s >> m;
+    vector<pair<int, int>> vec(n);
+    for (int i = 0; i < n; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+        vec[i] = {l, r};
+    }
+    int mx = vec[0].first;
+    for (int i = 1; i < n; i++)
+    {
+        mx = max(mx, vec[i].first - vec[i - 1].second);
+    }
+    mx = max(mx, m - vec[n - 1].second);
+ 
+    if(mx >= s) cout<<"YES\n";
+    else cout<<"NO\n";
+}
+ 
+int32_t main()
+{
+    fastIO;
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        CoderSanskar();
+    }
+    return 0;
+}
