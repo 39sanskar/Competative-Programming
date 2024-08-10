fa8187d1bcaed7e2ae9f5fc3882506bf9ee4868c
Sanskar Mishra <152903486+39sanskar@users.noreply.github.com>
2024-08-10 17:35:09 +0530

    Update Problem15.cpp
    D.Slavics_Exam.cpp

--- Week 13/Problem15.cpp
+++ Week 13/Problem15.cpp
@@ -1,1 +1,59 @@
-
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define fastIO                   \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);               \
+    cout.tie(NULL);              \
+    cout.precision(numeric_limits<double>::max_digits10);
+ 
+ 
+void CoderSanskar()
+{
+    string s, t;
+    cin >> s >> t;
+    int i = 0, n = s.size(), m = t.size();
+    for (int j = 0; j < m; j++)
+    {
+        while (i < n && s[i] != t[j])
+        {
+            if (s[i] == '?')
+            {
+                s[i] = t[j];
+                break;
+            }
+            i++;
+        }
+        if (i < n && s[i] == t[j])
+        {
+            i++;
+        }
+        else
+        {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+    while (i < n)
+    {
+        if (s[i] == '?')
+            s[i] = 'a';
+        i++;
+    }
+    cout << s << '\n';
+}
+ 
+int32_t main()
+{
+    fastIO;
+ 
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        CoderSanskar();
+    }
+    return 0;
+}
